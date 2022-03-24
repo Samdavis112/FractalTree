d51baa9062e999266466eab4a0ae339d68e155a2
Sam Davis <93987384+Samdavis112@users.noreply.github.com>
2022-03-24 23:21:49 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,11 +9,14 @@ function setup() {
     slider = createSlider(0, (2 * Math.PI), Math.PI / 4, 0.01);
     slider2 = createSlider(0, 5, 0, 1);
     slider3 = createSlider(50, 200, 0, 1);
-    let myCanvas = createCanvas(600, 600);
+    let myCanvas = createCanvas(600, 500);
     myCanvas.parent("BoxForCanvas");
     slider.addClass("slider");
-    slider.addClass("slider");
-    slider.addClass("slider");
+    slider2.addClass("slider");
+    slider3.addClass("slider");
+    slider.parent("sliders");
+    slider2.parent("sliders");
+    slider3.parent("sliders");
 }
 
 function draw() {
@@ -22,7 +25,7 @@ function draw() {
     maxLength = slider3.value();
     background(51);
     stroke(255);
-    translate(350, 500);
+    translate(300, 500);
     branch(200 - maxLength);
 }
 
